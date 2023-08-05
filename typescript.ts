37c3cda21b985c1dd72663d70140c5829efba103
Zarnab Raza <141522632+ZarnabRaza@users.noreply.github.com>
2023-08-05 13:20:24 -0700

    Add files via upload

---
+++ typescript.ts
@@ -0,0 +1,32 @@
+console.log("Hey Abu Hurairah, I have started learning Typescript.");
+
+var myName:string = ("Zarnab");
+console.log("my Name is " , myName);
+
+var num1:number = 345;
+var num2:number = 346;
+var num3:number = 347;
+var num4:number = 348;
+var num5:number = 349;
+var num6:number = 350;
+var num7:number = 351;
+var num8:number = 352;
+var num9:number = 353;
+var num10:number = 354;
+
+var sum:number = (num1 + num2 + num3 + num4 + num5 + num6 + num7 + num8 + num9 + num10);
+
+
+var sub:number = (num1 - num2 - num3 - num4 - num5 - num6 - num7 - num8 - num9 - num10);
+
+
+var mul:number = (num1 * num2 * num3 * num4 * num5 * num6 * num7 * num8 * num9 * num10);
+
+
+var div:number = (num10/num8);
+
+
+console.log("Sum is = " + sum);
+console.log("Sub is =" + sub);
+console.log("Mul is =" + mul);
+console.log("Div is =" + div);
